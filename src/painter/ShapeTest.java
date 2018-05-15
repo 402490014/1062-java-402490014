9a05294e2bd4845dd4c2fe08809225cae44e3a65
402490014 <s0990419@gmail.com>
2018-05-15 17:23:26 +0800

    Initial commit for w11 demo

---
+++ src/painter/ShapeTest.java
@@ -0,0 +1,12 @@
+package painter;
+
+public class ShapeTest {
+
+	public static void main(String[] args) {
+		PainterPanel app = new PainterPanel();
+		app.setSize(800, 600);
+		app.setVisible(true);
+
+	}
+
+}
